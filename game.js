2a0fadfd820b6445e9b988c1c983130259e7bbf5
Vina Jacob <vinamariej@gmail.com>
2022-01-01 18:53:14 -0800

    Add more rockets and end game if rockets hit

--- game.js
+++ game.js
@@ -40,25 +40,79 @@ function create() {
 
   // Rockets
   rockets = this.physics.add.group();
-  var rocketPink1 = rockets.create(150, 100, "rocket-pink");
-  var rocketGreen1 = rockets.create(300, 100, "rocket-green");
+  var rocketGreen1 = rockets.create(350, 100, "rocket-green");
+  var rocketPink1 = rockets.create(75, 100, "rocket-pink");
+  var rocketGreen2 = rockets.create(-50, 225, "rocket-green");
+  var rocketPink2 = rockets.create(225, 225, "rocket-pink");
+  var rocketGreen3 = rockets.create(350, 350, "rocket-green");
+  var rocketPink3 = rockets.create(75, 350, "rocket-pink");
+
+  rocketGreen1.setName("greenRocket1");
+  rocketPink1.setName("pinkRocket1");
+  rocketGreen2.setName("greenRocket2");
+  rocketPink2.setName("pinkRocket2");
+  rocketGreen3.setName("greenRocket3");
+  rocketPink3.setName("pinkRocket3");
+
+  rockets.setVelocityX(50);
 
   // Character
   player = this.physics.add.sprite(216, 444, "ep");
-
   player.setCollideWorldBounds(true);
+  player.setSize(12, 12);
+  this.physics.add.collider(player, rockets, hitRocket, null, this);
 
   cursors = this.input.keyboard.createCursorKeys();
 }
 
+function hitRocket(player, rocket) {
+  this.physics.pause();
+
+  player.anims.play("turn");
+
+  gameOver = true;
+}
+
+function checkInWorld(scene, rocket) {
+  return Phaser.Geom.Rectangle.Overlaps(scene.scene.physics.world.bounds, rocket.getBounds());
+}
+
+function repositionRocket(scene, rocket) {
+  if (!checkInWorld(scene, rocket)) {
+    rocket.body.x = -110;
+  }
+}
+
 function update() {
+  player.setDamping(true);
+  const dragValue = 0.15;
+
+  repositionRocket(this.scene, rockets.getMatching("name", "greenRocket1")[0]);
+  repositionRocket(this.scene, rockets.getMatching("name", "pinkRocket1")[0]);
+  repositionRocket(this.scene, rockets.getMatching("name", "greenRocket2")[0]);
+  repositionRocket(this.scene, rockets.getMatching("name", "pinkRocket2")[0]);
+  repositionRocket(this.scene, rockets.getMatching("name", "greenRocket3")[0]);
+  repositionRocket(this.scene, rockets.getMatching("name", "pinkRocket3")[0]);
+
+  // Left/right
   if (cursors.left.isDown) {
     player.setVelocityX(-160);
   } else if (cursors.right.isDown) {
     player.setVelocityX(160);
-  } else if (cursors.up.isDown) {
+  } else if (cursors.left.isUp) {
+    player.setDragX(dragValue);
+  } else if (cursors.right.isUp) {
+    player.setDragX(dragValue);
+  }
+
+  // Up/down
+  if (cursors.up.isDown) {
     player.setVelocityY(-160);
   } else if (cursors.down.isDown) {
     player.setVelocityY(160);
+  } else if (cursors.up.isUp) {
+    player.setDragY(dragValue);
+  } else if (cursors.down.isUp) {
+    player.setDragY(dragValue);
   }
 }
